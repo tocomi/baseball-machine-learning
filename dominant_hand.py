fa821b3a0f1e6f952c8d21acf8dde4facdecc16e
tocomi <tocomi@wish.ocn.ne.jp>
2019-03-30 22:47:36 +0900

    initial commit

---
+++ dominant_hand.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn import datasets, tree, metrics
+
+
+'''
+投手の左右別被打率をもとに投手の利き腕を判別する
+'''
+def main():
+
+    df = pd.read_csv('./data/dominant_hand.csv')
+
+    # df.locで列を抽出
+    # df.valuesでDataFrameを配列に変換
+    averages = df.loc[ :, [ 'right', 'left' ]].values
+    hands = df[ 'hand' ].values
+
+    # 教師データの割合を設定
+    sample_size = len(df.values)
+    train_size = int(sample_size * 3 / 5)
+
+    # 識別器の作成と学習
+    classifier = tree.DecisionTreeClassifier()
+    classifier.fit(averages[:train_size], hands[:train_size])
+
+    # 利き腕を判別する
+    expected = hands[train_size:]
+    predicted = classifier.predict(averages[train_size:])
+
+    # 判定の適合率を表示
+    print('Accuracy: \n', metrics.accuracy_score(expected, predicted))
+
+if __name__ == '__main__':
+    main()
